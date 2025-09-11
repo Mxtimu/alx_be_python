86ba95b34e10741dac3137bfbf2e35a565f29368
mxtimu <matimujunior3@gmail.com>
2025-09-11 22:16:47 +0200

    Added the finance_calculator converted everything to ints at the end

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+montly_income = float(input("Enter your monthly income: "))
+total_monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+monthly_savings =  montly_income - total_monthly_expenses
+
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(f"Your monthly savings are ${int(monthly_savings)}")
+print(f"Projected Savings after one year, with interest, is: ${int(projected_savings)}")
