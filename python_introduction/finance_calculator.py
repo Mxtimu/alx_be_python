afa650ae3f5b4c02e86276be82571d719ce45f15
mxtimu <matimujunior3@gmail.com>
2025-09-11 22:19:58 +0200

    logic worked well, just had a few spelling mistakes

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-montly_income = float(input("Enter your monthly income: "))
-total_monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_savings =  montly_income - total_monthly_expenses
+monthly_savings =  monthly_income - monthly_expenses
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
