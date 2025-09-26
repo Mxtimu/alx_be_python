8519e7eaca3041d0dda8c63044df38eb9810b4dd
mxtimu <matimujunior3@gmail.com>
2025-09-26 20:28:26 +0200

    the checker wanted the function to specifically have num1, num2 and operation. I added the data types which the checker does not accept

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
 
     #parameters to use and works as an input the get user input :
 
